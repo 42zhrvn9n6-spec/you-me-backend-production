8368ac4f460c2c96c91ce19bb05b4722bee9f445
42zhrvn9n6-spec <42zhrvn9n6@privaterelay.appleid.com>
2026-08-11 16:05:35 +0300

    Delete subscription.test.ts

--- subscription.test.ts
+++
@@ -1,2 +0,0 @@
-import { isValidWebhookAuth } from '../src/services/revenuecat.service';
-test('development webhook can operate without secret',()=>expect(isValidWebhookAuth(undefined)).toBe(true));
